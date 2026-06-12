e35fbc93d0ad1ce62f6ba657c48430193bcede7d
zong-t <zhouzhongtao2022@163.com>
2026-06-12 15:57:54 +0800

    fix(vnc.sh): support UEFI boot and fix critical bugs
    - Auto detect BIOS/UEFI firmware and install correct grub variant
    - Move dd wipe before setup-alpine to prevent GPT partition table destruction
    - Fix PermitRootLogin sed not matching unhashed config line
    - Fix printf newline not expanding in double quotes
    - Fix brace syntax error
    - Compress to under 2000 chars for VNC paste

--- vnc.sh
+++ vnc.sh
@@ -1,11 +1,7 @@
 sh << 'EOF'
-#!/bin/sh
-# Alpine VNC Auto Install
-P="yiwan123"
-D="/dev/vda"
-R="http://mirrors.aliyun.com/alpine/v3.23"
-setup-interfaces -a -r
-sleep 2
+P="yiwan123";D="/dev/vda";R="http://mirrors.aliyun.com/alpine/v3.23"
+[ -d /sys/firmware/efi ]&&FW=uefi||FW=bios;echo $FW
+apk add gdisk >/dev/null 2>&1;sgdisk -Z $D 2>/dev/null;dd if=/dev/zero of=$D bs=512 count=34 2>/dev/null;B=$(basename $D);S=$(cat /sys/block/$B/size);dd if=/dev/zero of=$D bs=512 seek=$((S-34)) count=34 2>/dev/null;sync;sleep 1;setup-interfaces -a -r;sleep 2
 cat>/tmp/a<<E
 KEYMAPOPTS="us us"
 HOSTNAMEOPTS="-n alpine"
@@ -22,32 +18,8 @@ NTPOPTS="-c chrony"
 USEROPTS="-a -k none"
 DISKOPTS="-m sys -s 0 $D"
 E
-export ERASE_DISKS="$D"
-echo|setup-alpine -f /tmp/a -e
-mkdir -p /mnt
-mount ${D}2 /mnt
-mount ${D}1 /mnt/boot
-echo "root:$P"|chroot /mnt chpasswd
-sed -i 's/#PermitRootLogin.*/PermitRootLogin yes/' /mnt/etc/ssh/sshd_config
-printf "$R/main\n$R/community\n">/mnt/etc/apk/repositories
-mount --bind /dev /mnt/dev
-mount --bind /proc /mnt/proc
-mount --bind /sys /mnt/sys
-chroot /mnt apk update
-chroot /mnt apk add util-linux grub grub-bios curl vim bash parted e2fsprogs
-umount /mnt/boot
-swapoff -a 2>/dev/null
-dd if=/dev/zero of=$D bs=1K seek=32 count=992 conv=notrunc
-dd if=/dev/zero of=$D bs=512 seek=1 count=33 conv=notrunc
-B=$(basename $D)
-S=$(cat /sys/block/$B/size)
-dd if=/dev/zero of=$D bs=512 seek=$((S-33)) count=33 conv=notrunc
-sync
-mount ${D}1 /mnt/boot
-chroot /mnt grub-install --recheck $D
-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
-chroot /mnt apk del syslinux 2>/dev/null
-sync
-umount /mnt/boot /mnt/dev /mnt/proc /mnt/sys /mnt 2>/dev/null
-reboot
+export ERASE_DISKS=$D;echo|setup-alpine -f /tmp/a -e;mkdir -p /mnt
+if [ "$FW" = "uefi" ];then V=$(blkid -t TYPE=vfat -o device|grep "^$D"|head -1);X=$(blkid -t TYPE=ext4 -o device|grep "^$D"|tail -1);[ -z "$V" ]&&V=${D}1;[ -z "$X" ]&&X=${D}2;mount $X /mnt;mkdir -p /mnt/boot/efi;mount $V /mnt/boot/efi;else mount ${D}2 /mnt;mount ${D}1 /mnt/boot;fi
+echo "root:$P"|chroot /mnt chpasswd;sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/' /mnt/etc/ssh/sshd_config;echo -e "$R/main\n$R/community">/mnt/etc/apk/repositories;mount --bind /dev /mnt/dev;mount --bind /proc /mnt/proc;mount --bind /sys /mnt/sys;[ "$FW" = "uefi" ]&&{ modprobe efivarfs 2>/dev/null;mount --bind /sys/firmware/efi /mnt/sys/firmware/efi 2>/dev/null;}
+chroot /mnt apk update;chroot /mnt apk add util-linux curl vim bash e2fsprogs;if [ "$FW" = "uefi" ];then chroot /mnt apk add grub-efi efibootmgr dosfstools;chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot/efi --removable;else chroot /mnt apk add grub grub-bios;chroot /mnt grub-install --recheck $D;fi;chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg;chroot /mnt apk del syslinux 2>/dev/null;sync;[ "$FW" = "uefi" ]&&umount /mnt/boot/efi 2>/dev/null||umount /mnt/boot 2>/dev/null;umount /mnt/dev /mnt/proc /mnt/sys /mnt 2>/dev/null;reboot
 EOF
